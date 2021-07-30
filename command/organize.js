ab38c2180667c4d074b6b839a0ba00c810af1e9b
Abhinav Mahalley <abhinavmahalley22.com>
2021-07-31 00:46:23 +0530

    Types updated in organize file

--- command/organize.js
+++ command/organize.js
@@ -3,10 +3,11 @@ let path=require("path");
 
 
 let types = {
-    media: ["mp4", "mkv"],
+    media: ['mp3',"mp4", "mkv",'mov','m4a', 'm4v', 'mpg','mpeg', 'wmv', 'avi', 'flv', '3gp', '3gpp', '3g2', '3gp2'],
     archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', "xz"],
     documents: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex'],
-    app: ['exe', 'dmg', 'pkg', "deb"]
+    app: ['exe', 'dmg', 'pkg', "deb"],
+    images:['jpeg','jpg','bmp','png','gif','raw','eps','cr2','nef','orf','sr2','tiff','tif']
 }
 
 
